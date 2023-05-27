d7862fdaf9573ab22d7d539743c4f792367bdd43
Mwaa Joseph <joseph.mwaa@getsote.com>
2023-05-27 22:09:11 +0300

    Use gunicorn for flask deployment

--- voice_ai/app.py
+++ voice_ai/app.py
@@ -52,6 +52,3 @@ def verify():
         # delete_file(redeemingVoice)
         return { 'verified': True }
     return { 'verified': False }    
-
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=os.environ.get("FLASK_SERVER_PORT"), debug=True)
